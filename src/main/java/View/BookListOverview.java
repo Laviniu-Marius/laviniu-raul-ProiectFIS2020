0a4e6466abcf31e0cf10051c42fc8d250caf97df
Raul <raul.scorobete99@gmail.com>
2020-06-29 23:38:46 +0300

    [F2-6]

--- src/main/java/View/BookListOverview.java
+++ src/main/java/View/BookListOverview.java
@@ -55,7 +55,19 @@ public class BookListOverview {
         });
 
         BookAddButton.setOnAction(e ->{
-            BookController.addBook();
+            String[] ret=BookContentOverview.display("Add Book","Enter details");
+            if(ret!=null) {
+                BookController.addBook(ret[0], ret[1], ret[2]);
+                list.clear();
+                BookController.readBooks();
+                for (Book b : BookController.bookList) {
+                    list.add(b.getTitle());
+                }
+
+                listView.getItems().clear();
+                listView.getItems().addAll(list);
+            }
+
         });
 
         grid.getChildren().addAll(listView,BookAddButton,BookDeleteButton,BackButton,ViewButton);
