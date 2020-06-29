1d2530fa32556708df43c61b72235f13141cebdc
Raul <raul.scorobete99@gmail.com>
2020-06-29 21:07:44 +0300

    Created read and write book functions

---
+++ src/main/java/Controllers/BookController.java
@@ -0,0 +1,42 @@
+package Controllers;
+
+import Objects.Book;
+import View.Alert;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class BookController {
+    public static List<Book> bookList;
+    public static String path="Books.JSON";
+
+    public static void readBooks () {
+        ObjectMapper objectMapper=new ObjectMapper();
+
+        try {
+            bookList = objectMapper.readValue(new File(path), new TypeReference<List<Book>>(){});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void writeBooks(){
+        ObjectMapper objectMapper=new ObjectMapper();
+
+        try {
+            objectMapper.writerWithDefaultPrettyPrinter().writeValue(new File(path), bookList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Book getBook(String title){
+        return bookList.stream().filter(e -> title.equals(e.getTitle())).findAny().orElse(null);
+
+    }
+
+
+
+}
