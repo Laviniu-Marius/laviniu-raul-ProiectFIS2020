6ad653cd7477274dcfa1ac613712c5c3b6323706
Laviniu-Marius <laviniu_rus@yahoo.com>
2020-06-29 13:26:22 +0300

    Added Book class

---
+++ src/main/java/Objects/Book.java
@@ -0,0 +1,62 @@
+package Objects;
+
+import java.util.Objects;
+
+public class Book {
+
+    private String title, author, content;
+
+    public Book () {
+
+    }
+
+    public Book(String title,String author, String content) {
+        this.title = title;
+        this.author = author;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(title, book.title) &&
+                Objects.equals(author, book.author) &&
+                Objects.equals(content, book.content);
+    }
+
+}
